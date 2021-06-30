ae48152332f4dcf929d4c3e3c564bf288309abf6
Anthony Nguyen <nvhoanganh1909@gmail.com>
2021-06-30 14:37:12 +1000

    remove all

--- src/Application/Common/Models/PaginatedList.cs
+++ src/Application/Common/Models/PaginatedList.cs
@@ -1,5 +1,4 @@
-﻿using Microsoft.EntityFrameworkCore;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -24,13 +23,5 @@ namespace CER.Application.Common.Models
         public bool HasPreviousPage => PageIndex > 1;
 
         public bool HasNextPage => PageIndex < TotalPages;
-
-        public static async Task<PaginatedList<T>> CreateAsync(IQueryable<T> source, int pageIndex, int pageSize)
-        {
-            var count = await source.CountAsync();
-            var items = await source.Skip((pageIndex - 1) * pageSize).Take(pageSize).ToListAsync();
-
-            return new PaginatedList<T>(items, count, pageIndex, pageSize);
-        }
     }
 }
